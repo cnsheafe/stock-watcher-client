cf9e55aaba45e9962a9e60b4e1842737c5684742
cnsheafe <cnsheafe@gmail.com>
2017-11-21 19:11:00 -0700

    Add nav tabs and search suggestions
    Reuse old components for searching stock tickers.

--- src/components/TickerPage.tsx
+++ src/components/TickerPage.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
 import TickerList from "./TickerList";
-import { TickerSearch } from "./TickerSearch";
+import Search from "./Search";
+import SuggestionsList from "./SuggestionsList";
+import PageTabs from './PageTabs';
+
 export default class extends React.Component {
   render() {
-    return [<TickerSearch />, <TickerList />];
+    return [<PageTabs /> ,<Search />, <SuggestionsList />, <TickerList />];
   }
 }
