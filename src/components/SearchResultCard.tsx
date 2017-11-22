129c4e97a7eebe394e3d275bbc4c74d1f06abe2b
cnsheafe <cnsheafe@gmail.com>
2017-11-22 10:14:16 -0700

    Add style

--- src/components/SearchResultCard.tsx
+++ src/components/SearchResultCard.tsx
@@ -3,6 +3,7 @@ import { Company } from "../store/schema";
 import store from '../store/store';
 import { addGraphAsync } from '../store/actions';
 import { Ticker } from '../actions/Tickers';
+import '../styles/suggestion-card.scss';
 
 interface ResultCardProps {
   suggestionHandler: (company: Company, Tickers: Set<Ticker>) => (any),
@@ -16,7 +17,7 @@ export default class SearchResultCard extends React.Component<ResultCardProps, {
   render() {
     const company = this.props.company;
     return (
-      <div onClick={e => this.props.suggestionHandler(company, this.props.tickers)}>
+      <div onClick={e => this.props.suggestionHandler(company, this.props.tickers)} className="suggestion-card">
         <i className="material-icons purple700">add_circle</i>
         <p>
           {company.symbol}: {company.name}
