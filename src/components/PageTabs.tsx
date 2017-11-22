177fdf7afb9ae77a832eb9e32ed494eabbcf7eab
cnsheafe <cnsheafe@gmail.com>
2017-11-22 14:11:10 -0700

    Add handlers to change state

--- src/components/PageTabs.tsx
+++ src/components/PageTabs.tsx
@@ -1,17 +1,34 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
+import TabSwitch from "../actions/TabSwitch";
+import store from "../store/store";
+
 import "../styles/page-tabs.scss";
 export default class PageTabs extends React.Component {
+  tabSwitch: TabSwitch;
+  constructor() {
+    super(null);
+    this.tabSwitch = new TabSwitch();
+  }
   render() {
     return [
       <ul className="page-tabs">
         <li>
-          <Link to="/">Tickers</Link>
+          <Link to="/" onClick={e => this.switchHandler(true)}>
+            Tickers
+          </Link>
         </li>
         <li>
-          <Link to="/graphs">Graphs</Link>
+          <Link to="/graphs" onClick={e => this.switchHandler(false)}>
+            Graphs
+          </Link>
         </li>
       </ul>
     ];
+  }
+
+  switchHandler(toTicker: boolean) {
+    console.log("switch!");
+    store.dispatch(this.tabSwitch.switchPage(toTicker));
   }
 }
