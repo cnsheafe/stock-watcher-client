78564d4a5faee478125a9b56adfd28da641d0973
cnsheafe <cnsheafe@gmail.com>
2017-11-22 12:37:04 -0700

    Add conditional rendering and self-polling
    Displays a loading animation when ticker is not done fetching. Additionally sets up polling to get new prices minutely.

--- src/components/TickerCard.tsx
+++ src/components/TickerCard.tsx
@@ -1,42 +1,69 @@
 import * as React from "react";
 import Tickers from "../actions/Tickers";
 import store from "../store/store";
-import '../styles/tickers.scss';
+import "../styles/tickers.scss";
 
 interface TickerProps {
   symbol: string;
   price: number;
-  index: number;
 }
 export class TickerCard extends React.Component<TickerProps> {
-  mTickers: Tickers;
   timerId;
-  constructor(props) {
-    super(props);
-    this.mTickers = new Tickers();
-  }
+  mTickers: Tickers;
+
+    constructor(props: TickerProps) {
+      super(props);
+      this.mTickers = new Tickers();
+    }
   render() {
+
+    const priceElm =
+      this.props.price === 0 ? (
+        <span className="loading-anim">
+          <div className="line" />
+          <div className="line" />
+          <div className="line" />
+        </span>
+      ) : (
+        <span>{this.props.price} USD</span>
+      );
     return (
       <div className="ticker-card">
         <span>{this.props.symbol.toUpperCase()}</span>
-        <span>{this.props.price} USD</span>
-        <i onClick={e => this.removeTicker(this.props.index)} className="material-icons">
-        cancel
+        {priceElm}
+        <i
+          onClick={e => this.removeTicker(this.props.symbol)}
+          className="material-icons"
+        >
+          cancel
         </i>
       </div>
     );
   }
-  removeTicker(index: number) {
-    console.log("MyIndex: ",index);
-    store.dispatch(this.mTickers.RemoveOne(index));
+  removeTicker(symbol: string) {
+    store.dispatch(this.mTickers.RemoveOne(symbol));
   }
+
   componentDidMount() {
+    console.log(this.props.symbol);
+    this.updateHandler(this.props.symbol);
     this.timerId = setInterval(() => {
-      console.log("Update!");
-      store.dispatch(this.mTickers.UpdateOne(this.props.index, this.props.symbol));
+      store.dispatch(
+        new Tickers().UpdateOne(this.props.symbol)
+      );
     }, 1e3 * 60);
   }
   componentWillUnmount() {
     clearInterval(this.timerId);
+  }
+
+  updateHandler(symbol: string) {
+    store.dispatch(
+      new Tickers().UpdateOne(symbol)
+    ).then(val => {
+      if(!val) {
+        this.updateHandler(symbol);
+      }
+    });
   }
 }
