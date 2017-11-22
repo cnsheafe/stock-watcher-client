55654a747d9c7eaabe9650ed8bfa5fd102ca6e38
cnsheafe <cnsheafe@gmail.com>
2017-11-21 21:11:01 -0700

    Add update method for tickers

--- src/actions/Tickers.ts
+++ src/actions/Tickers.ts
@@ -3,15 +3,21 @@ import { IState } from "../store/store";
 import { fetchPrices } from "../services/fetchPrices";
 export const ADD_TICKERS = "ADD_TICKERS";
 export const REMOVE_TICKER = "REMOVE_TICKER";
+export const UPDATE_TICKER = "UPDATE_TICKER";
 
 export type Ticker = {
   symbol: string;
   price: number;
 };
 
+export interface UpdateTicker {
+  type: "UPDATE_TICKER",
+  tickerIndex: number,
+  updatedPrice: number
+}
 export interface AddTickers {
   type: "ADD_TICKERS";
-  tickers: Ticker[];
+  tickers: Set<Ticker>;
 }
 
 export interface RemoveTicker {
@@ -24,11 +30,12 @@ export default class Tickers {
     return function(dispatch: Dispatch<IState>) {
       fetchPrices(symbols).then(results => {
         console.log(results);
-        const tickers = symbols.map(symbol => {
-          return {
+        const tickers = new Set<Ticker>();
+        symbols.forEach(symbol => {
+          tickers.add({
             symbol: symbol.toLowerCase(),
             price: results[symbol][0].price
-          };
+          });
         });
         dispatch<AddTickers>({
           type: ADD_TICKERS,
@@ -45,5 +52,18 @@ export default class Tickers {
         tickerIndex: index
       });
     };
+  }
+  UpdateOne(index: number, symbol: string) {
+    return function(dispatch: Dispatch<IState>) {
+       fetchPrices([symbol]).then(results => {
+         const updatedPrice = results[symbol][0].price;
+
+      dispatch<UpdateTicker>({
+        type: UPDATE_TICKER,
+        tickerIndex: index,
+        updatedPrice: updatedPrice
+      });
+    });
+    }
   }
 }
