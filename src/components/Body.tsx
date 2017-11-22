c01ba11025de5faf09dd4fc490094816daabc3c3
cnsheafe <cnsheafe@gmail.com>
2017-11-21 19:59:45 -0700

    Add page nav and search list

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,15 +1,17 @@
 import * as React from "react";
 
 import Search from "./Search";
+import SearchPage from './SearchPage';
 import Graphs from "./Graphs";
 import Modal from "./Modal";
-
+import PageTabs from './PageTabs';
 // Presentation Component for the functionality of the app
 export class Body extends React.Component {
   render() {
     return(
       <section>
-        <Search />
+        <PageTabs />
+        <SearchPage />
         <Graphs />
         <Modal />
       </section>
