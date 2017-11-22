474a7580caa7a38e110e78c6b462916db197e544
cnsheafe <cnsheafe@gmail.com>
2017-11-21 19:08:53 -0700

    Add tickers to props
    Helps check that tickers are not added if they already exist.

--- src/components/SearchResultCard.tsx
+++ src/components/SearchResultCard.tsx
@@ -2,16 +2,21 @@ import * as React from "react";
 import { Company } from "../store/schema";
 import store from '../store/store';
 import { addGraphAsync } from '../store/actions';
+import { Ticker } from '../actions/Tickers';
 
 interface ResultCardProps {
-  suggestionHandler: (company: Company) => (any),
+  suggestionHandler: (company: Company, Tickers: Set<Ticker>) => (any),
+  tickers: Set<Ticker>
   company: Company
 }
 export default class SearchResultCard extends React.Component<ResultCardProps, {}> {
+  constructor(props) {
+    super(props);
+  }
   render() {
     const company = this.props.company;
     return (
-      <div onClick={e => this.props.suggestionHandler(company)}>
+      <div onClick={e => this.props.suggestionHandler(company, this.props.tickers)}>
         <i className="material-icons purple700">add_circle</i>
         <p>
           {company.symbol}: {company.name}
