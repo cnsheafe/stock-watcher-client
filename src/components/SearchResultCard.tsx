b2d6e1eeadf077efb75a569374fe4c719cec9e87
cnsheafe <cnsheafe@gmail.com>
2017-11-22 14:11:44 -0700

    Add conditional rendering based on page state

--- src/components/SearchResultCard.tsx
+++ src/components/SearchResultCard.tsx
@@ -1,28 +1,59 @@
 import * as React from "react";
 import { Company } from "../store/schema";
-import store from '../store/store';
-import { addGraphAsync } from '../store/actions';
-import { Ticker } from '../actions/Tickers';
-import '../styles/suggestion-card.scss';
+import store from "../store/store";
+import "../styles/suggestion-card.scss";
+import Tickers, { Ticker } from "../actions/Tickers";
+import SearchSuggestions from "../actions/SearchSuggestions";
+import Graphs from '../actions/Graphs';
 
 interface ResultCardProps {
-  suggestionHandler: (company: Company, Tickers: Set<Ticker>) => (any),
-  tickers: Set<Ticker>
-  company: Company
+  tickers: Set<Ticker>;
+  company: Company;
+  onTickers: boolean;
 }
-export default class SearchResultCard extends React.Component<ResultCardProps, {}> {
+export default class SearchResultCard extends React.Component<
+  ResultCardProps,
+  {}
+> {
   constructor(props) {
     super(props);
   }
   render() {
     const company = this.props.company;
-    return (
-      <div onClick={e => this.props.suggestionHandler(company, this.props.tickers)} className="suggestion-card">
+    const cardElm = this.props.onTickers ? (
+      <div
+        onClick={e => this.tickerHandler(company, this.props.tickers)}
+        className="suggestion-card"
+      >
+        <i className="material-icons purple700">add_circle</i>
+        <p>
+          {company.symbol}: {company.name}
+        </p>
+      </div>
+    ) : (
+      <div
+        onClick={e => this.graphHandler(company)}
+        className="suggestion-card"
+      >
         <i className="material-icons purple700">add_circle</i>
         <p>
           {company.symbol}: {company.name}
         </p>
       </div>
     );
+    return cardElm;
+  }
+  graphHandler(company: Company) {
+    console.log("Hello");
+    store.dispatch(new Graphs().addGraph(company));
+  }
+  tickerHandler(company: Company, inUseTickers: Set<Ticker>) {
+    for (let ticker of inUseTickers) {
+      if (ticker.symbol === company.symbol.toLowerCase()) {
+        return;
+      }
+    }
+    store.dispatch(new Tickers().RequestMany([company.symbol]));
+    store.dispatch(new SearchSuggestions().clearSuggestions());
   }
 }
