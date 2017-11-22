ce892a35d7c3777732c6293b1bd7414ce68b8c5c
cnsheafe <cnsheafe@gmail.com>
2017-11-22 16:33:25 -0700

    Add unit test for actions

---
+++ src/__tests__/SearchSuggestions.test.ts
@@ -0,0 +1,53 @@
+import SearchSuggestions, {
+  SEARCH_RESULT,
+  CLEAR_RESULT
+} from "../actions/SearchSuggestions";
+import { IState } from "../store/store";
+import { Dispatch } from "redux";
+
+describe("SearchSuggestions", function() {
+  const mock: Dispatch<IState> = function(action) {
+    return action;
+  };
+
+  describe("fetchSuggestions", function() {
+    test("it should return object of type SearchResults", function() {
+      const searchphrase = "blahblah";
+
+      const result = new mockSuggestions().fetchSuggestions(searchphrase);
+      result(mock).then(action => {
+        console.log(action);
+        expect(action).toEqual({
+          type: SEARCH_RESULT,
+          results: [{ symbol: "msft", name: "Microsoft" }]
+        });
+      });
+    });
+  });
+  describe("clearSuggestions", function() {
+    test("it should return object of type ClearSearchResults", function() {
+      const result = new mockSuggestions().clearSuggestions();
+      const newResult = result(mock);
+      expect(newResult.type).toBe(CLEAR_RESULT);
+    });
+  });
+});
+
+class mockSuggestions extends SearchSuggestions {
+  fetchSuggestions(searchphrase: any) {
+    return super.fetchSuggestions(searchphrase);
+  }
+  clearSuggestions() {
+    return super.clearSuggestions();
+  }
+  protected fetchCompanies(searchphrase: string) {
+    return new Promise((resolve, reject) => {
+      resolve([
+        {
+          symbol: "msft",
+          name: "Microsoft"
+        }
+      ]);
+    });
+  }
+}
---
+++ src/__tests__/Tickers.test.ts
@@ -0,0 +1,67 @@
+import { Dispatch } from "redux";
+import { IState } from "../store/store";
+
+import Tickers, {
+  UPDATE_TICKER,
+  REQUEST_TICKERS,
+  REMOVE_TICKER,
+  Ticker
+} from "../actions/Tickers";
+
+describe("Tickers", function() {
+  const mock: Dispatch<IState> = function(action) {
+    return action;
+  };
+
+  describe("RequestMany", function() {
+    test("it should return object of type RequestTickers", function() {
+      const result = new Tickers().RequestMany(["msft", "amd"]);
+      console.log(result(mock));
+      const mockTickers: Ticker[] = [
+        { symbol: "msft", price: 0 },
+        { symbol: "amd", price: 0 }
+      ];
+      expect(result(mock).type).toBe(REQUEST_TICKERS);
+      expect(result(mock).tickers).toEqual(new Set<Ticker>(mockTickers));
+    });
+  });
+
+  describe("RemoveOne", function() {
+    test("it should return object of type RemoveTicker", function() {
+      const result = new Tickers().RemoveOne("msft")(mock);
+      expect(result).toEqual({
+        type: REMOVE_TICKER,
+        symbol: "msft"
+      });
+    });
+  });
+
+  describe("UpdateOne", function() {
+    test("it should return object of type UpdateTicker", function() {
+      const result = new mockTickers().UpdateOne("msft");
+      result(mock).then(actions => {
+        console.log(actions);
+        expect(actions).toEqual({
+          type: UPDATE_TICKER,
+          updatedTickers: {
+            symbol: "msft",
+            price: 10
+          }
+        });
+      });
+    });
+  });
+});
+
+class mockTickers extends Tickers {
+  UpdateOne(symbol: string) {
+    return super.UpdateOne(symbol);
+  }
+  protected fetchPrices(symbol: string) {
+    return new Promise((resolve, reject) => {
+      resolve({
+        msft: [{ price: 10 }]
+      });
+    });
+  }
+}
