517b1891f1e9311623387cb4d1b31dc2d88bc15c
cnsheafe <cnsheafe@gmail.com>
2017-11-22 10:13:59 -0700

    Add style

--- src/components/PageTabs.tsx
+++ src/components/PageTabs.tsx
@@ -1,10 +1,17 @@
 import * as React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "../styles/page-tabs.scss";
 export default class PageTabs extends React.Component {
   render() {
     return [
-      <Link to="/">Tickers</Link>,
-      <Link to="/graphs">Graphs</Link>
+      <ul className="page-tabs">
+        <li>
+          <Link to="/">Tickers</Link>
+        </li>
+        <li>
+          <Link to="/graphs">Graphs</Link>
+        </li>
+      </ul>
     ];
   }
 }
