ed95cf7a3c14fbca9d8b2ccdd8af9c818a8d88e8
cnsheafe <cnsheafe@gmail.com>
2017-11-24 08:56:57 -0700

    Comment out
    Going to make organization changes to reducer testing.

--- src/store/reducer.test.ts
+++ src/store/reducer.test.ts
@@ -1,122 +1,112 @@
-import { reducer, IState } from "./store";
+// import { reducer, IState } from "./store";
 
-import {
-  SEARCH,
-  ADD_GRAPH,
-  REM_GRAPH,
-  TOGGLE_MODAL,
-  ListSearchResults,
-  AddGraph,
-  RemoveGraph,
-  ToggleModalDisplay
-} from "./actions";
 
-import { Company } from "./schema";
-import { Ticker } from '../actions/Tickers';
+// import { Company } from "./schema";
+// import { Ticker } from '../actions/Tickers';
 
-// Prologue
-// The reducer should accept incoming actions
-//  and return the updated state correctly
+// // Prologue
+// // The reducer should accept incoming actions
+// //  and return the updated state correctly
 
-describe("When the reducer", () => {
-  let mockState: IState;
-  beforeEach(() => {
-    mockState = {
-      searchResults: [],
-      graphs: [],
-      showModal: false,
-      modalSymbol: "",
-      tickers: new Set<Ticker>(),
-      onTickers: true
-    };
-  });
+// describe("When the reducer", () => {
+//   let mockState: IState;
+//   beforeEach(() => {
+//     mockState = {
+//       searchResults: [],
+//       graphs: [],
+//       showModal: false,
+//       modalSymbol: "",
+//       tickers: new Set<Ticker>(),
+//       onTickers: true
+//     };
+//   });
 
-  describe(`receives a ${SEARCH} action`, () => {
+//   describe(`receives a ${SEARCH} action`, () => {
 
-    it("should update the searchResults", () => {
-      const companies: Array<Company> = [
-        {
-          symbol: "msft",
-          name: "Microsoft"
-        },
-        {
-          symbol: "amd",
-          name: "AMD"
-        }
-      ];
+//     it("should update the searchResults", () => {
+//       const companies: Array<Company> = [
+//         {
+//           symbol: "msft",
+//           name: "Microsoft"
+//         },
+//         {
+//           symbol: "amd",
+//           name: "AMD"
+//         }
+//       ];
 
-      const newState: IState = reducer(mockState, ListSearchResults(companies));
-      expect(newState.searchResults).toHaveLength(2);
-      expect(newState.searchResults).toEqual(companies);
-    });
-  });
+//       const newState: IState = reducer(mockState, ListSearchResults(companies));
+//       expect(newState.searchResults).toHaveLength(2);
+//       expect(newState.searchResults).toEqual(companies);
+//     });
+//   });
 
-  describe(`receives a ${ADD_GRAPH} action`, () => {
-    it("should add a new graph to the list", () => {
-      const newGraph: AddGraph = {
-        type: ADD_GRAPH,
-        company: {
-          symbol: "msft",
-          name: "Microsoft"
-        },
-        dataPoints: [1, 2, 3, 4, 5],
-        labels: ["1", "2", "3", "4", "5"]
-      };
+//   describe(`receives a ${ADD_GRAPH} action`, () => {
+//     it("should add a new graph to the list", () => {
+//       const newGraph: AddGraph = {
+//         type: ADD_GRAPH,
+//         company: {
+//           symbol: "msft",
+//           name: "Microsoft"
+//         },
+//         dataPoints: [1, 2, 3, 4, 5],
+//         labels: ["1", "2", "3", "4", "5"]
+//       };
 
-      const newState:IState = reducer(mockState, newGraph);
-      expect(newState.graphs).toHaveLength(1);
-      expect(newState.graphs[0].index).toBe(0);
-      expect(newState.graphs[0].graphId).toBe("graph0");
-      expect(newState.graphs[0].company).toEqual(newGraph.company);
-      expect(newState.graphs[0].dataset).toEqual(newGraph.dataPoints);
-      expect(newState.graphs[0].labels).toEqual(newGraph.labels);
-    });
-  });
+//       const newState:IState = reducer(mockState, newGraph);
+//       expect(newState.graphs).toHaveLength(1);
+//       expect(newState.graphs[0].index).toBe(0);
+//       expect(newState.graphs[0].graphId).toBe("graph0");
+//       expect(newState.graphs[0].company).toEqual(newGraph.company);
+//       expect(newState.graphs[0].dataset).toEqual(newGraph.dataPoints);
+//       expect(newState.graphs[0].labels).toEqual(newGraph.labels);
+//     });
+//   });
 
-  describe(`receives a ${REM_GRAPH} action`, () => {
-    let newGraph: AddGraph;
-    beforeAll(() => {
-      newGraph = {
-        type: ADD_GRAPH,
-        company: {
-          symbol: "msft",
-          name: "Microsoft"
-        },
-        dataPoints: [1, 2, 3, 4, 5],
-        labels: ["1", "2", "3", "4", "5"]
-      };
+//   describe(`receives a ${REM_GRAPH} action`, () => {
+//     let newGraph: AddGraph;
+//     beforeAll(() => {
+//       newGraph = {
+//         type: ADD_GRAPH,
+//         company: {
+//           symbol: "msft",
+//           name: "Microsoft"
+//         },
+//         dataPoints: [1, 2, 3, 4, 5],
+//         labels: ["1", "2", "3", "4", "5"]
+//       };
 
-      mockState = reducer(mockState, newGraph);
-    });
+//       mockState = reducer(mockState, newGraph);
+//     });
 
-    it("should remove an existing graph", () => {
-      const removeGraph: RemoveGraph = {
-        type: REM_GRAPH,
-        graphId: "graph0"
-      }
-      const newState: IState = reducer(mockState, removeGraph);
-      expect(newState.graphs).toHaveLength(0);
-    });
-  });
+//     it("should remove an existing graph", () => {
+//       const removeGraph: RemoveGraph = {
+//         type: REM_GRAPH,
+//         graphId: "graph0"
+//       }
+//       const newState: IState = reducer(mockState, removeGraph);
+//       expect(newState.graphs).toHaveLength(0);
+//     });
+//   });
 
-  describe(`receives a ${TOGGLE_MODAL} action`, () => {
-    let newAction: ToggleModalDisplay;
-    beforeEach(() => {
-      newAction = {
-        type: TOGGLE_MODAL
-      };
-    });
+//   describe(`receives a ${TOGGLE_MODAL} action`, () => {
+//     let newAction: ToggleModalDisplay;
+//     beforeEach(() => {
+//       newAction = {
+//         type: TOGGLE_MODAL
+//       };
+//     });
 
-    it("should toggle state property showModal", () => {
-      const newState: IState = reducer(mockState, newAction);
+//     it("should toggle state property showModal", () => {
+//       const newState: IState = reducer(mockState, newAction);
 
-      expect(newState.showModal).toBe(true);
-    });
+//       expect(newState.showModal).toBe(true);
+//     });
 
-    it("should store modal stock symbol, ", () => {
-      newAction.symbol = "msft";
-      const newState: IState = reducer(mockState, newAction);
-      expect(newState.modalSymbol).toBe("msft");
-    });
-  });
-});
+//     it("should store modal stock symbol, ", () => {
+//       newAction.symbol = "msft";
+//       const newState: IState = reducer(mockState, newAction);
+//       expect(newState.modalSymbol).toBe("msft");
+//     });
+//   });
+// });
