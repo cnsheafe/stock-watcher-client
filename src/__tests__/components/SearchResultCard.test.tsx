7bd26d2cb78cc028be4ebe9e710685d9c48a5a19
cnsheafe <cnsheafe@gmail.com>
2017-11-26 14:38:42 -0700

    Add render tests

---
+++ src/__tests__/components/SearchResultCard.test.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import SearchResultCard from "../../components/SearchResultCard";
+
+import * as Enzyme from "enzyme";
+import * as Adapter from "enzyme-adapter-react-16";
+import { Ticker } from "../../actions/Tickers";
+import { Graph, Company } from "../../store/schema";
+
+(Enzyme as any).configure({ adapter: new Adapter() });
+
+describe("SearchResultCard", function() {
+  const mockTickers: Set<Ticker> = new Set<Ticker>([
+    {
+      symbol: "msft",
+      price: 10.0
+    },
+    {
+      symbol: "amd",
+      price: 5.0
+    }
+  ]);
+  const mockCompany = {
+    symbol: "msft",
+    name: "Microsoft"
+  };
+
+  test("should render a div with a i and p", function() {
+    const wrapper = Enzyme.render(
+      <SearchResultCard
+        tickers={mockTickers}
+        company={mockCompany}
+        onTickers={true}
+      />
+    );
+    expect(wrapper.first().hasClass('suggestion-card')).toBe(true);
+    expect(wrapper.find('i')).toHaveLength(1);
+    expect(wrapper.find('p')).toHaveLength(1);
+  });
+});
