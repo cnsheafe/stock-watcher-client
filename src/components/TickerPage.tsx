0365c8fca48d6c58262fda64412d858aa5bfba9c
cnsheafe <cnsheafe@gmail.com>
2017-11-25 20:42:43 -0700

    Remove nav
    Nav is now at index

--- src/components/TickerPage.tsx
+++ src/components/TickerPage.tsx
@@ -2,10 +2,9 @@ import * as React from "react";
 import TickerList from "./TickerList";
 import Search from "./Search";
 import SuggestionsList from "./SuggestionsList";
-import PageTabs from './PageTabs';
 
 export default class extends React.Component {
   render() {
-    return [<PageTabs /> ,<Search />, <SuggestionsList />, <TickerList />];
+    return [<Search />, <SuggestionsList />, <TickerList />];
   }
 }
