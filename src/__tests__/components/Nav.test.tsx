75280409a4f567a87388cc88f24be77abe2d0faa
cnsheafe <cnsheafe@gmail.com>
2017-11-25 21:43:00 -0700

    Add test for nav

---
+++ src/__tests__/components/Nav.test.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import Nav from "../../components/Nav";
+import { NavLink } from "react-router-dom";
+
+// import Enzyme, { shallow, mount, ShallowWrapper } from "enzyme";
+import * as Enzyme from "enzyme";
+import * as Adapter from "enzyme-adapter-react-16";
+
+(Enzyme as any).configure({ adapter: new Adapter() });
+// Enzyme.configure({adapter: Adapter});
+
+describe("Nav", function() {
+  test("should render a list with 2 links", function() {
+    const wrapper = Enzyme.shallow(<Nav />);
+    expect(wrapper.find("ul")).toHaveLength(1);
+    expect(wrapper.find("li")).toHaveLength(2);
+    expect(wrapper.find("ul").hasClass("page-tabs")).toBe(true);
+    expect(
+      wrapper
+        .find("li")
+        .first()
+        .hasClass("ticker-link")
+    ).toBe(true);
+    expect(
+      wrapper
+        .find("li")
+        .last()
+        .hasClass("graph-link")
+    ).toBe(true);
+  });
+});
