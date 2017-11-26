b76a28a977b658b0c65872afe016bd6ef6ff2b19
cnsheafe <cnsheafe@gmail.com>
2017-11-26 14:51:29 -0700

    Add render test

---
+++ src/__tests__/components/TickerList.test.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { TickerList } from "../../components/TickerList";
+
+import * as Enzyme from "enzyme";
+import * as Adapter from "enzyme-adapter-react-16";
+import { Ticker } from "../../actions/Tickers";
+import { Graph, Company } from "../../store/schema";
+
+(Enzyme as any).configure({ adapter: new Adapter() });
+
+describe("TickerList", function() {
+  const mockTickers: Set<Ticker> = new Set<Ticker>([
+    {
+      symbol: "msft",
+      price: 10.0
+    },
+    {
+      symbol: "amd",
+      price: 5.0
+    }
+  ]);
+  test("should render a div containing a div and ul", function() {
+    const wrapper = Enzyme.shallow(<TickerList tickers={mockTickers} />);
+    expect(
+      wrapper
+        .find("div")
+        .first()
+        .find("div")
+        .exists()
+    );
+    expect(wrapper.find("ul")).toHaveLength(1);
+  });
+  test("should render ul with 2 li", function() {
+    const wrapper = Enzyme.shallow(<TickerList tickers={mockTickers} />);
+
+    expect(wrapper.find("li")).toHaveLength(2);
+  });
+});
